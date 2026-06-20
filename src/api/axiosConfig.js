4d678bcc62327952b459385bd9c5a9e6915d2164
mateocles <mateobermudez871@gmail.com>
2026-06-19 22:49:43 -0500

    change interceptor

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { ENV } from "../config/env";
 import { STORAGE_KEYS } from "../config/storageKeys";
+import { useAuthStore } from "../store/useAuthStore";
 
 const api = axios.create({
   baseURL: ENV.API_BASE_URL,
@@ -14,5 +15,19 @@ api.interceptors.request.use((config) => {
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+
+// Si el token expira o es inválido (401/403) cierra la sesión: el router
+// redirige automáticamente al login al quedar el token en null.
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+    const hadToken = !!useAuthStore.getState().token;
+    if (hadToken && (status === 401 || status === 403)) {
+      useAuthStore.getState().logout();
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
